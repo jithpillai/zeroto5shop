e7a2fc174da2ea14820497dc4990f201011f7fd5
Prijith Pillai <prijithkumar.sreedharan@oracle.com>
2023-02-07 12:24:40 +0530

    Adding resource file

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function showPopup() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function hidePopup() {
+    document.getElementById("overlay").style.display = "none";
+}
---
+++ js/fire.js
@@ -0,0 +1,21 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCQ9Z12jygMkbFsbkS9nFJ7iv_CM-5wgZM",
+  authDomain: "zeroto5.firebaseapp.com",
+  projectId: "zeroto5",
+  storageBucket: "zeroto5.appspot.com",
+  messagingSenderId: "197139932655",
+  appId: "1:197139932655:web:1ac467de964a14de13c21a",
+  measurementId: "G-50VX47VMK5"
+};
+
+// Initialize Firebase
+/* const app = firebase.initializeApp(firebaseConfig);
+
+const sugForm = firebase.database().ref('sugForm');
+
+document.getElementById('sugFormId').addEventListener('submit', 'onSuggestionSubmit');
+
+function onSuggestionSubmit(e) {
+    debugger
+    e.preventDefault();
+}; */
